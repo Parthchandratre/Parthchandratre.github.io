43dfbf3803f887218497ea5baa9e8529dba13a7d
Parthchandratre <65350629+Parthchandratre@users.noreply.github.com>
2020-05-19 13:45:11 +0530

    upload index.html instead

---
+++ Portfolio/js/main.js
@@ -0,0 +1,19 @@
+//Owl-carousel
+
+$('.site-main .about-area .owl-carousel').owlCarousel({
+  loop:true,
+  autoplay:true,
+  dots:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    544:{
+      items:3
+    }
+  }
+})
+
+let nav_offset_top        
+
+
